6871d87976ca37392520c09fdcf51d195e909cf1
Stephan Seitz <stephan.seitz@fau.de>
2024-04-27 14:20:53 +0200

    fix: handle the case that git-repo regex does not match
    `Regex.exec` returns `null` when the regex does not match.
    In this case print then return the resolved value directly
    when our parsing fails.
    
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
 const { join } = require('node:path');
 const { readFileSync } = require('node:fs');
 
-const lockfile = join(process.env.GITHUB_WORKSPACE, 'package-lock.json');
-const matcher = /.+\/(?<repo>[\w-]+\/[\w-]+)\.git#(?<hash>[0-9a-f]+)/;
+const lockfile = join(process.env.GITHUB_WORKSPACE ?? process.cwd(), 'package-lock.json');
+const matcher = /.+github\.com\/(?<repo>[\w-]+\/[\w-]+)\.git#(?<hash>[0-9a-f]+)/;
 
-function getBody() {
+/** @param {import('@actions/core')} core */
+function getBody(core) {
   const { packages } = JSON.parse(readFileSync(lockfile, 'utf8'));
   return Object.entries(packages).slice(1).map(([key, value]) => {
     if (value.resolved.startsWith('git')) {
-      const { repo, hash } = matcher.exec(value.resolved).groups;
-      return `- [${repo}@\`${hash}\`](https://github.com/${repo}/commit/${hash})`;
+      const match = matcher.exec(value.resolved)
+      if (match) {
+        // @ts-ignore
+        const { repo, hash } = match.groups;
+        return `- [${repo}@\`${hash}\`](https://github.com/${repo}/commit/${hash})`;
+      } else {
+        core.warning(`URL: ${value.resolved}`, { file: lockfile, title: 'Failed to parse URL' })
+        return `- ${value.resolved}`;
+      }
     }
     const name = key.substring(13), version = value.version;
     return `- [${name}@\`${version}\`](https://www.npmjs.com/package/${name}/v/${version})`;
   }).join('\n');
 }
 
-/** @param {import('@types/github-script').AsyncFunctionArguments} */
+/** @param {import('github-script').AsyncFunctionArguments} */
 module.exports = async function({core, exec}) {
   core.summary.addRaw('## Old versions', true);
-  core.summary.addRaw(getBody(), true).addEOL();
+  core.summary.addRaw(getBody(core), true).addEOL();
   await core.summary.write();
   await exec.exec('npm', ['update']);
 
-  const body = getBody();
+  const body = getBody(core);
   core.summary.addRaw('## New versions', true);
   core.summary.addRaw(body, true).addEOL();
   await core.summary.write();
